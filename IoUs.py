eae970dcbe6084cc0c6ca5c997b341acc521c99a
Vstorion <93765394+Vstorion@users.noreply.github.com>
2022-08-19 11:22:51 +0800

    Update IoUs.py
    mIoU & BIoU

--- IoUs.py
+++ IoUs.py
@@ -3,12 +3,12 @@ import cv2
 
 # size=(H,W)
 def fast_hist(a, b, n=2):
-    # a = label.flatten();b = pred.flatten();n is a real（在这里为19）
+    # a = label.flatten();b = pred.flatten();n is a real
     k = (a >= 0) & (a < n)  # k是一个一维bool数组，形状(H×W,)；目的是找出标签中需要计算的类别（去掉了背景）
     return np.bincount(n * a[k].astype(int) + b[k], minlength=n ** 2).reshape(n,\
                           n)  # np.bincount计算了从0到n**2-1这n**2个数中每个数出现的次数，返回值形状(n, n)
 
-def per_class_iu(hist):  # 分别为每个类别（在这里是19类）计算mIoU，hist的形状(n, n)
+def per_class_iu(hist):  # 分别为每个类别计算mIoU，hist的形状(n, n)
     return np.diag(hist) / (hist.sum(1) + hist.sum(0) - np.diag(
         hist))  # 矩阵的对角线上的值组成的一维数组/矩阵的所有元素之和，返回值形状(n,)
 
